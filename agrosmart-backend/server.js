fc9e7a026267e557913897f7692aae8443b68c42
Ashok Kumar Mahato <ashok79911@gmail.com>
2025-09-30 13:15:14 +0530

    Update server.js

--- agrosmart-backend/server.js
+++ agrosmart-backend/server.js
@@ -28,8 +28,8 @@ const PORT = process.env.PORT || 4000;
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log("✅ MongoDB connected");
-    // app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-    module.exports = app; 
+     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+   // module.exports = app; 
   })
   .catch(err => console.error("MongoDB connection error:", err));
 
